d9e6ed656440089873f51e563f8963dfa18ab0cd
ilayda <ilaydaozken@gmail.com>
2023-07-31 14:57:42 +0300

    Add project files.

---
+++ CasgemObservable/Dal/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace CasgemObservable.Dal
+{
+    public class Context :  IdentityDbContext<AppUser ,AppRole, int >
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AKOHCIC\\SQLEXPRESS;initial catalog=CasgemObservable;integrated Security=true");
+        }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<UserProcess> UserProceses { get; set; }
+        public DbSet<WelcomeMessage> WelcomeMessages { get; set; }
+    }
+}
---
+++ CasgemObservable/Dal/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace CasgemObservable.Dal
+{
+    public class Discount
+    {
+        public int DiscountID { get; set; } 
+        public string DiscountCode { get; set; } 
+        public decimal DiscountAmount { get; set; } 
+        public bool DiscountCodeStatus { get; set; } 
+    }
+}
